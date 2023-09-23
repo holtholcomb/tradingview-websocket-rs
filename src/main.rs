ea334c0e9ba9ed002a8b1ebafe6c13747e86ff43
Brandon Ros <brandonros1@gmail.com>
2023-09-23 15:46:07 -0400

    websocketerror was required for error + send

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
 mod websocket;
-mod tradingview_api;
+mod trading_view_api;
 
 use std::error::Error;
 use std::sync::mpsc;
 
-use tradingview_api::TradingviewApi;
+use trading_view_api::TradingViewApi;
 use websocket::WebSocket;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let (incoming_tx, incoming_rx) = mpsc::channel();
     let (outgoing_tx, outgoing_rx) = mpsc::channel();
-    let _ = std::thread::spawn(move || -> Result<(), Box<dyn Error + Send>> {
-        let tradingview_api = TradingviewApi::new(incoming_rx, outgoing_tx)?;
-        tradingview_api.handler()
+    let trading_view_thread = std::thread::spawn(move || {
+        let trading_view_api = TradingViewApi::new(incoming_rx, outgoing_tx)?;
+        trading_view_api.handler()
     });
-    let mut websocket = WebSocket::new(incoming_tx, outgoing_rx)?;
-    websocket.handle_stream()?;
+    let websocket_thread = std::thread::spawn(move || {
+        let mut websocket = WebSocket::new(incoming_tx, outgoing_rx)?;
+        websocket.handle_stream()
+    });
+    let trading_view_result = trading_view_thread.join().expect("TradingView thread panicked");
+    let websocket_result = websocket_thread.join().expect("Websocket thread panicked");
+    if let Err(err) = trading_view_result {
+        eprintln!("Error in TradingView thread: {:?}", err);
+    }
+    if let Err(err) = websocket_result {
+        eprintln!("Error in Websocket thread: {:?}", err);
+    }
     Ok(())
 }
--- src/websocket.rs
+++ src/websocket.rs
@@ -8,6 +8,47 @@ use native_tls::TlsConnector;
 trait ReadWrite: Read + Write {}
 impl<T: Read + Write + ?Sized> ReadWrite for T {}
 
+#[derive(Debug)]
+pub enum WebsocketError {
+    ReadError,
+    ChannelSendError,
+    ChannelReceiveError,
+    FrameEncodeError,
+    FrameDecodeError,
+    WriteError,
+    AddressParseError,
+    ConnectError,
+    TlsCreationError,
+    TlsConnectError,
+    StringConversionError
+}
+
+impl std::fmt::Display for WebsocketError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            WebsocketError::ReadError => write!(f, "Read error"),
+            WebsocketError::ChannelSendError => write!(f, "Channel send error"),
+            WebsocketError::ChannelReceiveError => write!(f, "Channel receive error"),
+            WebsocketError::FrameEncodeError => write!(f, "Frame encode error"),
+            WebsocketError::FrameDecodeError => write!(f, "Frame decode error"),
+            WebsocketError::WriteError => write!(f, "Write error"),
+            WebsocketError::AddressParseError => write!(f, "Address parse error"),
+            WebsocketError::ConnectError => write!(f, "Connect error"),
+            WebsocketError::TlsCreationError => write!(f, "TLS creation error"),
+            WebsocketError::TlsConnectError => write!(f, "TLS connect error"),
+            WebsocketError::StringConversionError => write!(f, "String conversion error"),
+        }
+    }
+}
+
+impl Error for WebsocketError {}
+
+impl From<WebsocketError> for Box<dyn std::error::Error + Send> {
+    fn from(error: WebsocketError) -> Self {
+        Box::new(error)
+    }
+}
+
 pub struct WebSocket {
     tls_stream: Box<dyn ReadWrite + Send + Sync + 'static>,
     incoming_tx: Sender<String>,
@@ -15,13 +56,13 @@ pub struct WebSocket {
 }
 
 impl WebSocket {
-    pub fn new(incoming_tx: Sender<String>, outgoing_rx: Receiver<Vec<String>>) -> Result<WebSocket, Box<dyn Error>> {
-        let addr = "data.tradingview.com:443".to_socket_addrs()?.next().unwrap();
-        let stream = TcpStream::connect(addr)?;
+    pub fn new(incoming_tx: Sender<String>, outgoing_rx: Receiver<Vec<String>>) -> Result<WebSocket, Box<dyn Error + Send>> {
+        let addr = "data.tradingview.com:443".to_socket_addrs().map_err(|_| WebsocketError::AddressParseError)?.next().unwrap();
+        let stream = TcpStream::connect(addr).map_err(|_| WebsocketError::ConnectError)?;
 
         // Establish a TLS connection
-        let connector = TlsConnector::new()?;
-        let mut tls_stream = connector.connect("data.tradingview.com", stream)?;
+        let connector = TlsConnector::new().map_err(|_| WebsocketError::TlsCreationError)?;
+        let mut tls_stream = connector.connect("data.tradingview.com", stream).map_err(|_| WebsocketError::TlsConnectError)?;
         
         // Perform the WebSocket handshake with the server manually.
         let request = "\
@@ -33,12 +74,12 @@ impl WebSocket {
             Sec-WebSocket-Version: 13\r\n\
             Origin: https://www.tradingview.com\r\n\
             \r\n";
-        tls_stream.write_all(request.as_bytes())?;
+        tls_stream.write_all(request.as_bytes()).map_err(|_| WebsocketError::WriteError)?;
 
         // Read the server's response to ensure it's a 101 Switching Protocols response.
-        let mut buffer = [0u8; 4096];
-        tls_stream.read(&mut buffer)?;
-        let response = std::str::from_utf8(&buffer)?;
+        let mut buffer = [0u8; 65536];
+        tls_stream.read(&mut buffer).map_err(|_| WebsocketError::ReadError)?;
+        let response = std::str::from_utf8(&buffer).map_err(|_| WebsocketError::StringConversionError)?;
         assert!(response.contains("101 Switching Protocols"));
 
         Ok(WebSocket { 
@@ -54,52 +95,64 @@ impl WebSocket {
         }
 
         let fin_and_opcode = buffer[0];
-        let is_text_frame = (fin_and_opcode & 0x0F) == 0x01;
+        let opcode = fin_and_opcode & 0x0F;
 
-        if !is_text_frame {
-            println!("{:02x?}", buffer);
-            return Err("Unsupported frame type".into());
-        }
+        match opcode {
+            0x01 => {  // Text frame
+                let mask_and_length_byte = buffer[1];
+                let (payload_length, header_size) = match mask_and_length_byte & 0x7F {
+                    0..=125 => (mask_and_length_byte as usize, 2), // Direct length encoding
+                    126 => {
+                        if buffer.len() < 4 {
+                            return Ok(None);  // Not enough data
+                        }
+                        (u16::from_be_bytes([buffer[2], buffer[3]]) as usize, 4)
+                    },
+                    127 => {
+                        if buffer.len() < 10 {
+                            return Ok(None);  // Not enough data
+                        }
+                        // Note: Since usize can be 32-bits on some platforms (like 32-bit systems), 
+                        // this can potentially be a problem if the length is greater than usize::MAX.
+                        // You might want to handle this scenario, e.g., by rejecting too-large messages.
+                        let length_bytes = [
+                            buffer[2], buffer[3], buffer[4], buffer[5], 
+                            buffer[6], buffer[7], buffer[8], buffer[9]
+                        ];
+                        (u64::from_be_bytes(length_bytes) as usize, 10)
+                    },
+                    _ => return Err("Invalid payload length format".into())
+                };
 
-        let mask_and_length_byte = buffer[1];
-        let (payload_length, header_size) = match mask_and_length_byte & 0x7F {
-            0..=125 => (mask_and_length_byte as usize, 2), // Direct length encoding
-            126 => {
-                if buffer.len() < 4 {
+                if buffer.len() < (header_size + payload_length) {
                     return Ok(None);  // Not enough data
                 }
-                (u16::from_be_bytes([buffer[2], buffer[3]]) as usize, 4)
-            },
-            127 => {
-                if buffer.len() < 10 {
-                    return Ok(None);  // Not enough data
+
+                // Drain the header bytes
+                for _ in 0..header_size {
+                    buffer.pop_front();
                 }
-                // Note: Since usize can be 32-bits on some platforms (like 32-bit systems), 
-                // this can potentially be a problem if the length is greater than usize::MAX.
-                // You might want to handle this scenario, e.g., by rejecting too-large messages.
-                let length_bytes = [
-                    buffer[2], buffer[3], buffer[4], buffer[5], 
-                    buffer[6], buffer[7], buffer[8], buffer[9]
-                ];
-                (u64::from_be_bytes(length_bytes) as usize, 10)
-            },
-            _ => return Err("Invalid payload length format".into())
-        };
 
-        if buffer.len() < (header_size + payload_length) {
-            return Ok(None);  // Not enough data
+                // Drain and collect the payload bytes
+                let payload_bytes: Vec<u8> = buffer.drain(0..payload_length).collect();
+                let payload_str = std::str::from_utf8(&payload_bytes)?;
+
+                Ok(Some(payload_str.to_string()))
+            }
+            0x88 => {  // Close frame
+                // Handle the close frame
+                // For example, if you want to print the status code:
+                let status_code = u16::from_be_bytes([buffer[2], buffer[3]]);
+                println!("Received close frame with status code: {}", status_code);
+                buffer.drain(0..4);  // Drain the entire frame, including status code
+                Ok(None)  // Or you can choose to return an error or another appropriate result
+            }
+            // Add handling for other frame types if needed...
+            _ => {
+                println!("{:02x?}", buffer);
+                Err("Unsupported frame type".into())
+            }
         }
-
-        // Drain the header bytes
-        for _ in 0..header_size {
-            buffer.pop_front();
-        }
-
-        // Drain and collect the payload bytes
-        let payload_bytes: Vec<u8> = buffer.drain(0..payload_length).collect();
-        let payload_str = std::str::from_utf8(&payload_bytes)?;
-
-        Ok(Some(payload_str.to_string()))
     }
 
     fn encode_websocket_text_frame(&self, data: &str) -> Result<Vec<u8>, Box<dyn Error>> {
@@ -150,12 +203,12 @@ impl WebSocket {
         Ok(frame)
     }
 
-    pub fn handle_stream(&mut self) -> Result<(), Box<dyn Error>> {
+    pub fn handle_stream(&mut self) -> Result<(), Box<dyn Error + Send>> {
         let mut rx_buffer = VecDeque::new();
 
         let mut temp_buffer = [0u8; 65536];
         loop {
-            let read_bytes = self.tls_stream.read(&mut temp_buffer)?;
+            let read_bytes = self.tls_stream.read(&mut temp_buffer).map_err(|_| WebsocketError::ReadError)?;
 
             if read_bytes == 0 {
                 break;  // The stream has closed or there's an error.
@@ -166,16 +219,16 @@ impl WebSocket {
             loop {
                 match self.decode_websocket_frame(&mut rx_buffer) {
                     Ok(Some(incoming_message)) => {
-                        self.incoming_tx.send(incoming_message)?;
-                        let outgoing_messages = self.outgoing_rx.recv()?;
+                        self.incoming_tx.send(incoming_message).map_err(|_| WebsocketError::ChannelSendError)?;
+                        let outgoing_messages = self.outgoing_rx.recv().map_err(|_| WebsocketError::ChannelReceiveError)?;
                         for outgoing_message in outgoing_messages {
                             println!("outgoing_message: {}", outgoing_message);
-                            let encoded_frame = self.encode_websocket_text_frame(&outgoing_message)?;
-                            self.tls_stream.write_all(&encoded_frame)?;
+                            let encoded_frame = self.encode_websocket_text_frame(&outgoing_message).map_err(|_| WebsocketError::FrameEncodeError)?;
+                            self.tls_stream.write_all(&encoded_frame).map_err(|_| WebsocketError::WriteError)?;
                         }
                     },
                     Ok(None) => break,  // Not enough data yet
-                    Err(e) => return Err(e.into()),
+                    Err(_) => return Err(WebsocketError::FrameDecodeError.into()),
                 }
             }
         }
